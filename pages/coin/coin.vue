ddc6c8af6b04a9da4cd8b2299ed282e3d3fb8b99
hnmylm1007 <272496849@qq.com>
2022-11-12 13:57:41 +0800

    Add files via upload

--- pages/coin/coin.vue
+++ pages/coin/coin.vue
@@ -4,33 +4,28 @@
 			<text class="text-white font-sm mb-2">当前金币</text>
 			<text class="font-weight-bold text-white" style="font-size: 60rpx;">50</text>
 		</view>
-
 		<view class="border-top border-light-secondary my-3"></view>
-
 		<view>
 			<text class="font-sm text-muted">请选择充值金币</text>
 		</view>
-
 		<view class="flex flex-wrap" style="margin-left: -20rpx;margin-right: -20rpx;">
-			<view style="width: 33.3%;box-sizing: border-box;" class="p-2" v-for="(item,index) in list" :key="index" @click="chooseCoin(index)">
-				<view v-if="item.price > 0" style="height: 130rpx;" class="border rounded flex flex-column align-center justify-center"
-				 :class=" activeIndex === index ? 'border-main' : '' ">
+			<view style="width: 33.3%;box-sizing: border-box;" class="p-2" v-for="(item,index) in list" :key="index"
+				@click="chooseCoin(index)">
+				<view v-if="item.price > 0" style="height: 130rpx;"
+					class="border rounded flex flex-column align-center justify-center"
+					:class=" activeIndex === index ? 'border-main' : '' ">
 					<view class="flex align-center">
 						<text class="iconfont text-warning mr-1">&#xe633;</text>
 						<text class="font-md font-weight-bold">{{item.coin}}</text>
 					</view>
 					<text class="font text-light-muted">￥{{item.price}}</text>
 				</view>
-
-
-				<view v-else style="height: 130rpx;" class="border rounded flex flex-column align-center justify-center">
+				<view v-else style="height: 130rpx;"
+					class="border rounded flex flex-column align-center justify-center">
 					<text class="font text-light-muted">自定义</text>
 				</view>
-
 			</view>
 		</view>
-
-
 		<view class="position-fixed left-0 bottom-0 right-0 border-top flex px-3 align-center" style="height: 100rpx;">
 			<view class="flex align-center">
 				<text class="iconfont text-warning mr-1">&#xe633;</text>
@@ -40,12 +35,10 @@
 				<text class="text-white font-md">去充值</text>
 			</view>
 		</view>
-
-
 		<uni-popup ref="popup" type="dialog">
-			<uni-popup-dialog mode="input" message="自定义充值" :duration="2000" placeholder="充值金额" inputType="number" :before-close="true" @close="close" @confirm="confirm"></uni-popup-dialog>
+			<uni-popup-dialog mode="input" message="自定义充值" :duration="2000" placeholder="充值金额" inputType="number"
+				:before-close="true" @close="close" @confirm="confirm"></uni-popup-dialog>
 		</uni-popup>
-
 	</view>
 </template>
 
@@ -114,7 +107,7 @@
 			 */
 			confirm(done, value) {
 				console.log(value);
-				if(!value){
+				if (!value) {
 					return uni.showToast({
 						title: '请输入要充值的金额',
 						icon: 'none'
